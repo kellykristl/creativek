169e0ada8ba76c3962013e6b3fd3f5eb9c865e3c
kellykristl <goddesswebdesign@gmail.com>
2017-04-25 09:22:57 -0600

    created milestone-1 requirements

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<title>Creative K Web Development and Design</title>
+	</head>
+	<body>
+		<h1 class="header">Purpose</h1>
+		<p>My personal portfolio is to promote my web design and developmemt skills.</p>
+
+		<h1 class="header">Audience</h1>
+		<p>My audience will be potential employers.</p>
+
+		<h1 class="header">Goal</h1>
+		<p>To obtain part-time to full-time employment.</p>
+
+		<h1 class="header">Persona</h1>
+		<p><strong>John Smith - developer and potential employer</strong></p>
+		<p>John is mid-to-late 30's and is a developer at the managerial level. He is looking for a well-rounded, full-stack developer to join his team. He is on a desktop iMac with high resolution, large monitor and super fast internet connection. This computer is at his workplace. </p>
+
+		<h1 class="header">Use Case</h1>
+		<p>John has limited time to review my personal portfolio, as he has hundreds of applicants for one position. My site needs to be clean, simple and quick with eye -catching graphics and upfront information about me. I only have a couple minutes of his time to convince him that I am the perfect match for the developer position within his company.</p>
+	</body>
+</html>

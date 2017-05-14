17834afaf149c45efd6fdaf5ae2fa4d4d0f61304
kellykristl <goddesswebdesign@gmail.com>
2017-05-13 19:33:11 -0600

    PWP Milestone 2-a

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<title>Creative K Design</title>
+	</head>
+	<body>
+		<h1 class="header">Content Strategy</h1>
+
+		<h2 class="header">Landing Page</h2>
+		<p>This will contain my logo and name at the top left corner and a nav bar across the top right. Underneath will be a photo of myself that fills the screen with wording on top that says "Welcome to my portfolio".</p>
+
+		<h2 class="header">About Me</h2>
+		<p>This section will contain a poloroid style picture with a blurb about myself, my skills and what I like to do.</p>
+
+		<h2 class="header">My Works</h2>
+		<p>This section will contain thumbnails of sites I have created that fill the screen. If the user clicks on the thumbnail, it will open a pop-up that contains a few larger images of the site with a blurb about the site and what programs were used to create it. </p>
+
+		<h2 class="header">Contact Me</h2>
+		<p>This section will contain a contact me email form.</p>
+		<p><img src="images/PWPdesktopwireframe.png"></p>
+		<p><img src="images/PWPmobilewireframe1.png">
+			<img src="images/PWPmobilewireframe2.png"></p>
+	</body>
+</html>

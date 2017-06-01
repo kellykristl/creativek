ab50fceae465b1f754752614511f80d1a0cd232d
kellykristl <goddesswebdesign@gmail.com>
2017-06-01 15:49:52 -0600

    updated form-validate.js

--- public_html/assets/js/form-validate.js
+++ public_html/assets/js/form-validate.js
@@ -12,29 +12,29 @@ $(document).ready(function(){
 		// rules define what is good/bad input
 		// each rule starts with the form input element's NAME attribute
 		rules: {
-			name: {
+			contactName: {
 				required: true
 			},
 			email: {
 				email: true,
 				required: true
 			},
-			message: {
+			comments: {
 				required: true,
 				maxlength: 2000
 			}
 		},
 
 		// error messages to display to the end user
-		messages: {
-			name: {
+		contactName: {
+			contactName: {
 				required: "Please enter your name."
 			},
 			email: {
 				email: "Please enter a valid email address.",
 				required: "Please enter a valid email address."
 			},
-			message: {
+			comments: {
 				required: "Please enter a message.",
 				maxlength: "2000 characters max."
 			}
